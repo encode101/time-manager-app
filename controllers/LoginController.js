e42b24dfe3d39b9af8acd7fd3633005fc044aaa1
rahulmishra <rahulmishra.hello@gmail.com>
2018-02-26 18:08:01 +0530

    removed stored credentials

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,7 +1,4 @@
-var LoginController = timeManager.controller('LoginController', function($scope, LoginService, $location){
-	$scope.loginEmailId = 'admin@admin.com';
-	$scope.password = 'admin';
-	
+var LoginController = timeManager.controller('LoginController', function($scope, LoginService, $location){	
 	$scope.handelLogin = function(){
 		if(LoginService.logIn($scope.loginEmailId, $scope.password)){
 			$location.path('dashboard');
